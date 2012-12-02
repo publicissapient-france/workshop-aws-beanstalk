6f1efdfd52c93819ac585969768d88a1a98d712f
Jean-Louis Rigau <jeanlouis.rigau@gmail.com>
2012-12-02 19:16:41 +0100

    Remove CloudBees TechEvent specifities.

--- src/test/java/org/springframework/samples/petclinic/selenium/HomePageSelenium.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.petclinic.selenium;
-
-public class HomePageSelenium extends SeleniumSauceLabSetUp {
-
-    public void test_home_page_should_have_correct_title() throws Exception {
-            driver.get(targetUrl);
-            assertEquals("PetClinic :: a Spring Framework demonstration", driver.getTitle());
-        }
-}
